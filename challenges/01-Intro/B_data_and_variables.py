7e5e769530e62338c76999c0757caa8510646ee9
Danielle Traphagen <dtrap@UnicornSpawn.local>
2014-01-14 14:52:41 -0800

    Further edits to the file for test pass completion.

--- challenges/01-Intro/B_data_and_variables.py
+++ challenges/01-Intro/B_data_and_variables.py
@@ -20,6 +20,8 @@ my_lucky_number = 7
 
 ## But Dav's lucky number is 9 (and he wrote the test)
 ## Redefine the value "correctly" below
+my_lucky_number = 9
+some_int = 8
 
 ## Only the most recent variable assignment matters
 print "Dav's lucky number:", my_lucky_number
