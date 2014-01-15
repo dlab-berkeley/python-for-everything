3b86016959c7287d5be32a25da9938fb2ced6dc0
Dav Clark <davclark@berkeley.edu>
2014-01-15 12:24:46 -0800

    Some function testing

---
+++ challenges/04-Functions/A_checking_again.py
@@ -0,0 +1,41 @@
+# We applied the same logic many times in the previous challenge!
+# Let's use functions to make our lives easier...
+
+sally_age = 12
+john_age = 21
+
+def can_drink(age):
+    return False # Again - make this depend on the age!
+
+print "Sally can drink?", can_drink(sally_age)
+print "John can drink?", can_drink(john_age)
+
+# Now define a more complex function that takes whether someone is drinking and
+# only checks ages if they are
+
+def enforce(name, age, is_drinking):
+    # Replace "pass" with some logic to test things out
+    pass
+    # When you check,
+    # print "Checking Name!"
+    # but you'll need to set the name
+
+## Note that we can define our data before or after a function
+
+alice_name = "Alice"
+alice_age = 20
+alice_is_drinking = True
+bob_name = "Bob"
+bob_age = 12
+bob_is_drinking = False
+charles_name = "Charles"
+charles_age = 22
+charles_is_drinking = True
+
+enforce(alice_name, alice_age, alice_is_drinking)
+## And so on - do this for Bob and Charles!
+
+## That should've been a LOT less typing!
+
+## When you're done, have a look at the tests, and see how the correspond to the
+# functions above
---
+++ challenges/04-Functions/test_A_checking_again.py
@@ -0,0 +1,22 @@
+import A_checking_again as A
+
+def test_can_drink():
+    '''This is a much clearer test now that we're using functions!'''
+    assert A.can_drink(21) == True
+    assert A.can_drink(22) == True
+    assert A.can_drink(20) == False
+
+def test_enforce(capsys):
+    A.enforce("John", 18, False)
+    A.enforce("Chuck", 18, True)
+    A.enforce("Bob", 21, True)
+    out, _ = capsys.readouterr()
+    lines = out.split('\n')
+    assert lines[0] == "Checking Chuck!"
+    assert lines[1] == "This isn't right!"
+    assert lines[2] == "Checking Bob!"
+    assert lines[3] == "OK!"
+    assert lines[4] == ""
+
+# Advanced users should note that the above tests could be made even more
+# efficient using the "parametrize" decorator from pytest!
