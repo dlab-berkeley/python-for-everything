aacc6126ee80cf3fee9ec0f87e2093c6ba107769
Dav Clark <davclark@berkeley.edu>
2014-05-25 21:32:37 -0700

    Making throw-away argument consistent

--- challenges/01-Intro/test_A_print_stuff.py
+++ challenges/01-Intro/test_A_print_stuff.py
@@ -6,5 +6,5 @@ def test_printing(capsys):
     # This is a little unorthodox, but we're doing Test-driven *learning*
     # Folks don't know about functions yet!
     import A_print_stuff
-    out, err = capsys.readouterr()
+    out, _ = capsys.readouterr() # We use _ to indicate a throw-away value
     assert out == "Hello Python!\n"
