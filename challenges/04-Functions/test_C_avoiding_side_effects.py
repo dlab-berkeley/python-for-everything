5452562fb6e596fdef6a5f385d3a5e6fb21f8375
Dav Clark <davclark@berkeley.edu>
2014-05-28 15:56:30 -0700

    Fixing more tests

--- challenges/04-Functions/test_C_avoiding_side_effects.py
+++ challenges/04-Functions/test_C_avoiding_side_effects.py
@@ -2,8 +2,8 @@ import pytest
 
 def test_exponentiate(capsys):
     # This is the last time, I swear!
-    import B_avoiding_side_effects as B
-    assert B.exponentiate(2,10) == 1024
+    import C_avoiding_side_effects as C
+    assert C.exponentiate(2,10) == 1024def test_exponentiate(capsys):
     out, _ = capsys.readouterr()
     assert out == ''
 
