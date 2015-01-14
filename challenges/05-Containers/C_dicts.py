db008e4791f68fe17acfb42adf36c7039611a96e
Thomas Kluyver <takowl@gmail.com>
2015-01-13 16:25:54 -0800

    Add to containers lesson and exercises

---
+++ challenges/05-Containers/C_dicts.py
@@ -0,0 +1,13 @@
+# Make a dictionary mapping names (strings) to ages (integers)
+
+# (uncomment the next line to get started)
+#people =
+
+
+# Make this function look up a person and return their age at their
+# next birthday. If the person doesn't exist in the dictionary, return
+# -1 instead.
+
+def age_next_birthday(name):
+    # Write code here
+    return
