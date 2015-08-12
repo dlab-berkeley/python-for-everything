7d0f5a2e7e18cc802fb843fb5c0e599c51f54c6b
Dillon Niederhut <dillon.niederhut@gmail.com>
2015-08-12 11:55:51 -0700

    initial commit

--- challenges/03-Conditionals/test_A_basic_conditionals.py
+++
@@ -1,9 +0,0 @@
-# Here to trigger an error
-import pytest
-
-import A_basic_conditionals as A
-
-def test_can_drink(capsys):
-    # Again being a bit funny to grab the print...
-    assert A.sally_can_drink == False # Using == False to get clearer errors!
-    assert A.john_can_drink == True
--- cheat-sheets/start-ipython.sh
+++
@@ -1,2 +0,0 @@
-# This is currently insecure! Until you change the password in the config!
-ipython notebook --config notebook-config.py
