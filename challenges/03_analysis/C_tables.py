be3bafe886efd588b2c886feeb9031f1b3e8c169
Dillon Niederhut <dillon.niederhut@gmail.com>
2016-02-11 18:33:43 -0800

    day 3 challenge finished

---
+++ challenges/03_analysis/C_tables.py
@@ -0,0 +1,17 @@
+#!/bin/env python
+
+# In this challenge you are going to practice basic pandas functionality
+# by using a table of peoples' heights and a table of cities' heights
+# to compute a variable representing 100 person's absolute height
+# above sea level
+
+# 1. Read in the first hundred lines of the csv located at
+# data/03_people as person_data
+
+
+# 2. Now, read in the csv file located at data/03_cities, merge it
+# into person_data and call it all_data
+
+
+# 3. Finally, create a new variable called "height_above_sea", and
+# write that data frame to disk at data/height_above_sea.csv
