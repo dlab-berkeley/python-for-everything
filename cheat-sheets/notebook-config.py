97c44c3ada4ff1295d9b92479af8d80f6ea83358
Dillon Niederhut <dillon.niederhut@gmail.com>
2016-03-03 11:53:53 -0800

    file cleanup

--- cheat-sheets/notebook-config.py
+++
@@ -1,7 +0,0 @@
-c = get_config()
-
-# Notebook config
-c.NotebookApp.certfile = u'/home/collaboratool/collaboratool-cert.pem'
-c.NotebookApp.ip = '*'
-c.NotebookApp.open_browser = False
-c.NotebookApp.password = u'sha1:72eecedd5231:9d66151455494288b82761447d2757836cc514b1'
