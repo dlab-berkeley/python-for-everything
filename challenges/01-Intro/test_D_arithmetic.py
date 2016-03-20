80483516d6e6c477bd8ea10ff6478e9076de0307
Dillon Niederhut <dillon.niederhut@gmail.com>
2016-03-19 17:40:11 -0700

    removing extra challenges; closes #12

--- challenges/01-Intro/test_D_arithmetic.py
+++
@@ -1,7 +0,0 @@
-import D_arithmetic as D
-
-def test_arithmetic():
-    assert D.the_answer == 42
-    assert D.exponentiate(2, 8) == 256
-    assert D.remainder == 1
-    assert D.fractional == 2.5
