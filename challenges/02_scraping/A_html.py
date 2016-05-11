cb6d772d6f1b0fca89cf9870be58a1c6e19cf6e2
akokai <ak@kaios.net>
2016-05-10 21:33:39 -0700

    Resolve issue #22 with challenge and test for 02/A_html

--- challenges/02_scraping/A_html.py
+++ challenges/02_scraping/A_html.py
@@ -16,6 +16,6 @@ text =
 
 ## 3. Pull out the site header, and join all of the elements in the head into a single text string.
 
-head = ' '.join(things_in_head_object)
+header = ' '.join(things_in_head_object)
 
 ## 4. Compare the length of the header to the length of the text - what do you notice? If you really want to blow your mind, open this up in a browser and compare the human-rendered version with the source code.
